5594042dc6ef65bc9503c6dc0e6ef2b8c8c862c7
NichloasChiu <NichloasChiu@outlook.com>
2023-12-23 19:09:46 +0800

    alter markdown-p

--- plugins/community-markdown-preview.lua
+++ plugins/community-markdown-preview.lua
@@ -7,14 +7,4 @@ return {
     build = function() vim.fn["mkdp#util#install"]() end,
   },
 
-  -- install with yarn or npm
-  -- {
-  --   "iamcco/markdown-preview.nvim",
-  --   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  --   build = "cd app && yarn install",
-  --   init = function()
-  --     vim.g.mkdp_filetypes = { "markdown" }
-  --   end,
-  --   ft = { "markdown" },
-  -- },
 }
